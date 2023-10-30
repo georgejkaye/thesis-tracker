a7e83105b4d5d27ec57b82b6281e7c7e1975a220
George Kaye <georgejkaye@gmail.com>
2023-10-30 21:49:11 +0000

    [feat] Make page look prettier

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,14 @@
 "use client"
 import { useEffect, useState } from "react"
 import { Commit, getCommitDatetime, getCommitWords, getCommits } from "./api"
+import { Manrope } from "next/font/google"
+
+const manrope = Manrope({
+    weight: ["400", "700"],
+    style: ["normal"],
+    subsets: ["latin"],
+    display: "swap",
+})
 
 export default function Home() {
     const [commits, setCommits] = useState<Commit[]>([])
@@ -19,24 +27,40 @@ export default function Home() {
     const dateStyle = `${sharedStyle} bg-blue-400 text-white`
     const wordsStyle = `${sharedStyle} bg-red-400 text-white`
     const pagesStyle = `${sharedStyle} bg-green-400 text-white`
+    const wordsPerPageStyle = `${sharedStyle} bg-pink-400 text-white`
     return (
-        <main>
-            <div className="text-center text-2xl m-10 fon">
+        <main className={manrope.className}>
+            <div className="text-center text-2xl m-10 mt-48">
+                <div className="text-4xl font-bold m-10">Thesis tracker</div>
                 {!mainCommit ? (
                     ""
                 ) : (
-                    <div>
-                        As of
-                        <span className={dateStyle}>
-                            {getCommitDatetime(mainCommit)}
-                        </span>
-                        the thesis has
-                        <span className={wordsStyle}>
-                            {getCommitWords(mainCommit)}
-                        </span>
-                        words and
-                        <span className={pagesStyle}>{mainCommit.pages}</span>
-                        pages.
+                    <div className="flex flex-col">
+                        <div className="mb-10">
+                            <span className={dateStyle}>
+                                {getCommitDatetime(mainCommit)}
+                            </span>
+                        </div>
+                        <div className="m-4">
+                            <span className={wordsStyle}>
+                                {getCommitWords(mainCommit)}
+                            </span>
+                            words
+                        </div>
+                        <div className="m-4">
+                            <span className={pagesStyle}>
+                                {mainCommit.pages}
+                            </span>
+                            pages
+                        </div>
+                        <div className="m-4">
+                            <span className={wordsPerPageStyle}>
+                                {(mainCommit.words / mainCommit.pages).toFixed(
+                                    3
+                                )}
+                            </span>
+                            words per page
+                        </div>
                     </div>
                 )}
             </div>
